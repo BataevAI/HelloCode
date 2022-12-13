ae43bee0598ef3e7dde6a82e6507fe26ec698293
Andrey Bataev <bataevai@gmail.com>
2022-12-13 22:42:58 +0400

    add conditions

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша") {
+Console.Write("Привет, красавица!");
+
+}
+
+else {
+Console.Write("Здарова, " + username);
+
+
+}
