c826054bda70bd95ace0e6e1dab73d3cdc203be3
Andrey Bataev <bataevai@gmail.com>
2022-12-16 20:31:30 +0400

    add exercise 1

---
+++ Seminar1/Home work1/Exs.1/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//a = -9 b = -3 -> max = -3
+
+
+
+Console.WriteLine("Введите первое целое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine("Введите второе целое число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2) {
+
+Console.WriteLine($"первое число: {number1} больше второго числа: {number2}");
+
+}
+
+else Console.WriteLine($"первое число: {number2} больше второго числа: {number1}");
+
