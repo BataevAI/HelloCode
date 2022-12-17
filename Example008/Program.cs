2c4f170b4c3d6e7a9feab21d5c5a9a45286f31b5
Andrey Bataev <bataevai@gmail.com>
2022-12-17 14:30:55 +0400

    add Example 008

---
+++ Example008/Program.cs
@@ -0,0 +1,69 @@
+﻿
+void FillArray (int []  collection) {
+
+    int length = collection.Length; 
+    int index = 0;
+
+    while(index < length) 
+    {
+
+        collection[index] = new Random().Next(1, 10);    
+
+        index ++;
+
+    }
+
+
+}
+
+void PrintArray (int [] col)
+{
+
+    for (int i = 0; i < col.Length; i++) 
+    {    
+        Console.Write(col[i]);
+        Console.Write(" ");
+
+    }
+
+}
+
+int IndexOf (int []  collection1, int find) 
+{
+
+        int count = collection1.Length;              
+        int index = 0;
+        int position = 0;
+
+            while(index < count) 
+            {
+
+                if (  collection1[index] == find ) {
+
+                position = index;
+                break;
+
+                    }   
+
+                    //else   Console.WriteLine("нет совпадений");
+                    index++;
+
+            }
+            
+return position;
+
+}
+
+int [] array = new int[10];
+ 
+FillArray(array);
+PrintArray(array);
+
+Console.WriteLine();
+Console.WriteLine(IndexOf(array, 4));
+
+//int pos = IndexOf(array, 4);
+
+//Console.WriteLine(pos);
+
+
